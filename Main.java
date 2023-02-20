fbe0a8563ea7cdb56f99fe77495b12641fb4ee13
Shota Napetvaridze <napetvaridze.shota@gmail.com>
2023-02-20 20:14:48 +0100

    Working with 500

--- Main.java
+++ Main.java
@@ -7,6 +7,7 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Main {
+
   public static void main(String[] args) throws IOException {
     int port = 0;
     String dir = "";
@@ -35,91 +36,76 @@ public class Main {
             BufferedReader br = new BufferedReader(isr);
 
             StringBuilder request = new StringBuilder();
-
-            String line; // Temp variable called line that holds one line a t a time of our message
+            String line; // Temp variable called line that holds one line at a time of our message
             line = br.readLine();
             while (!line.isBlank()) {
               request.append(line + "\r\n");
               line = br.readLine();
             }
-            // String[] parts = line.split(" ");
-            // String version = parts[1];
 
             System.out.println("--REQUEST--");
             System.out.println(request);
 
             String firsline = request.toString().split("\n")[0];
             String resource = firsline.split(" ")[1];
-            System.out.println(resource);
             OutputStream clientOutput = client.getOutputStream();
             FileInputStream file;
 
-            if (resource.equals("/archlinux")) {
-              file = new FileInputStream(dir + "/img/archlinux.png");
-              clientOutput.write("HTTP/1.1 200 OK\r\n".getBytes());
+            try {
+              if (resource.equals("/archlinux")) {
+                file = new FileInputStream(dir + "/img/archlinux.png");
+                clientOutput.write("HTTP/1.1 200 OK\r\n".getBytes());
+                clientOutput.write("\r\n".getBytes());
+                clientOutput.write(file.readAllBytes());
+              } else if (resource.equalsIgnoreCase("/home")) {
+                file = new FileInputStream(dir + "/html/home.html");
+                clientOutput.write("HTTP/1.1 200 OK\r\n".getBytes());
+                clientOutput.write("\r\n".getBytes());
+                clientOutput.write(file.readAllBytes());
+              } else if (resource.equalsIgnoreCase("/signup")) {
+                file = new FileInputStream(dir + "/html/signup.htm");
+                clientOutput.write("HTTP/1.1 200 OK\r\n".getBytes());
+                clientOutput.write("\r\n".getBytes());
+                clientOutput.write(file.readAllBytes());
+              } else if (resource.equalsIgnoreCase("/")) {
+                file = new FileInputStream(dir + "/html/first.html");
+                clientOutput.write("HTTP/1.1 200 OK\r\n".getBytes());
+                clientOutput.write("\r\n".getBytes());
+                clientOutput.write(file.readAllBytes());
+              } else if (resource.equalsIgnoreCase("/test")) {
+                clientOutput.write("HTTP/1.1 302 Found\r\n".getBytes());
+                clientOutput.write("\r\n".getBytes());
+                clientOutput.write("<h1>Found</h1>".getBytes());
+                clientOutput.write("<p>The requested URL was found click here to redirect</p>".getBytes());
+                String str = String.format("<a href='http://127.0.0.1:%d/rick'><button>Visit Rick</button></a>\r\n",
+                    port);
+                clientOutput.write(str.getBytes());
+                clientOutput.write("\r\n".getBytes());
+              } else if (resource.equalsIgnoreCase("/rick")) {
+                file = new FileInputStream(dir + "/html/302.html");
+                clientOutput.write("HTTP/1.1 200 OK\r\n".getBytes());
+                clientOutput.write("\r\n".getBytes());
+                clientOutput.write(file.readAllBytes());
+              } else {
+                file = new FileInputStream(dir + "/html/404.html");
+                clientOutput.write("HTTP/1.1 404 Not Found\r\n".getBytes());
+                clientOutput.write("\r\n".getBytes());
+                clientOutput.write(file.readAllBytes());
+              }
+              client.close();
+              System.err.println("Client connection closed!");
+
+            } catch (Exception e) {
+              // 500 Internal Server Error
+              file = new FileInputStream(dir + "/html/500.html");
+              clientOutput.write("HTTP/1.1 500 Server Error\r\n".getBytes());
               clientOutput.write("\r\n".getBytes());
               clientOutput.write(file.readAllBytes());
-              clientOutput.flush();
-            } else if (resource.equalsIgnoreCase("/home")) {
-              file = new FileInputStream(dir + "/html/home.html");
-              clientOutput.write("HTTP/1.1 200 OK\r\n".getBytes());
-              clientOutput.write("\r\n".getBytes());
-              clientOutput.write(file.readAllBytes());
-              clientOutput.flush();
-            } else if (resource.equalsIgnoreCase("/signup")) {
-              file = new FileInputStream(dir + "/html/signup.htm");
-              clientOutput.write("HTTP/1.1 200 OK\r\n".getBytes());
-              clientOutput.write("\r\n".getBytes());
-              clientOutput.write(file.readAllBytes());
-              clientOutput.flush();
-            } else if (resource.equalsIgnoreCase("/")) {
-              file = new FileInputStream(dir + "/html/first.html");
-              clientOutput.write("HTTP/1.1 200 OK\r\n".getBytes());
-              clientOutput.write("\r\n".getBytes());
-              clientOutput.write(file.readAllBytes());
-              clientOutput.flush();
-            } else if (resource.equalsIgnoreCase("/test")) {
-              clientOutput.write("HTTP/1.1 302 Found\r\n".getBytes());
-              clientOutput.write("\r\n".getBytes());
-              clientOutput.write("<h1>Found</h1>".getBytes());
-              clientOutput.write("<p>The requested URL was found click here to redirect</p>".getBytes());
-              String str = String.format("<a href='http://127.0.0.1:%d/rick'><button>Visit Rick</button></a>\r\n",
-                  port);
-              clientOutput.write(str.getBytes());
-              clientOutput.write("\r\n".getBytes());
-              clientOutput.flush();
-            } else if (resource.equalsIgnoreCase("/rick")) {
-              file = new FileInputStream(dir + "/html/302.html");
-              clientOutput.write("HTTP/1.1 200 OK\r\n".getBytes());
-              clientOutput.write("\r\n".getBytes());
-              clientOutput.write(file.readAllBytes());
-              clientOutput.flush();
-            } else {
-              file = new FileInputStream(dir + "/html/404.html");
-              clientOutput.write("HTTP/1.1 404 Not Found\r\n".getBytes());
-              clientOutput.write("\r\n".getBytes());
-              clientOutput.write(file.readAllBytes());
-              clientOutput.flush();
             }
-            client.close();
-            System.err.println("Client connection closed!");
 
           }
 
         }
-
-      } catch (Exception e) {
-        ServerSocket serverSocket = new ServerSocket(port);
-        Socket client = serverSocket.accept();
-        OutputStream clientOutput = client.getOutputStream();
-        FileInputStream file= new FileInputStream(dir + "/html/500.html");
-        clientOutput.write("HTTP/1.1 500 Server Error\r\n".getBytes());
-        clientOutput.write("\r\n".getBytes());
-        clientOutput.write(file.readAllBytes());
-        clientOutput.flush();
-        serverSocket.close();
-        file.close();
-        client.close();
 
       }
 
--- Test.java
+++ Test.java
@@ -1,47 +1,38 @@
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.io.OutputStreamWriter;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class Test {
+    public static void main(String[] args) throws Exception {
+        String url = "http://127.0.0.1:8080/myendpoint"; // URL of the server endpoint
+        String data = "param1=value1&param2=value2"; // POST data
 
-   public static void main(String[] args) throws Exception {
+        // Create a URL object and open an HTTP connection to the server
+        URL urlObj = new URL(url);
+        HttpURLConnection con = (HttpURLConnection) urlObj.openConnection();
 
-      // Define the URL of the endpoint you want to POST to
-      URL url = new URL("http://127.0.0.1:8888/uploads");
+        // Set the request method to POST and set other properties as needed
+        con.setRequestMethod("POST");
+        con.setDoOutput(true);
 
-      // Open a connection to the URL
-      HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        // Write the POST data to the request body
+        OutputStream os = con.getOutputStream();
+        os.write(data.getBytes());
+        os.flush();
+        os.close();
 
-      // Set the request method to POST
-      conn.setRequestMethod("POST");
+        // Read the response from the server
+        BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+        String inputLine;
+        StringBuffer response = new StringBuffer();
+        while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
+        }
+        in.close();
 
-      // Set the request headers
-      conn.setRequestProperty("Content-Type", "application/json");
-      conn.setRequestProperty("Accept", "application/json");
-
-      // Enable the connection's output
-      conn.setDoOutput(true);
-
-      // Create the request body
-      String requestBody = "{\"key1\":\"value1\",\"key2\":\"value2\"}";
-
-      // Write the request body to the connection's output stream
-      OutputStreamWriter writer = new OutputStreamWriter(conn.getOutputStream());
-      writer.write(requestBody);
-      writer.flush();
-
-      // Read the response from the connection's input stream
-      BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-      String line;
-      StringBuilder response = new StringBuilder();
-      while ((line = reader.readLine()) != null) {
-         response.append(line);
-      }
-      reader.close();
-
-      // Print the response
-      System.out.println(response.toString());
-   }
+        // Print the response
+        System.out.println(response.toString());
+    }
 }
